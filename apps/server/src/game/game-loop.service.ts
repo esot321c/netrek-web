bde4fc91b7676a4db8b95280664408b00741da15
esot321c <esot321c@gmail.com>
2026-05-08 15:56:43 -0700

    bugfix/countdown-timer

--- apps/server/src/game/game-loop.service.ts
+++ apps/server/src/game/game-loop.service.ts
@@ -256,6 +256,7 @@ export class GameLoopService implements OnModuleInit, OnModuleDestroy {
     this.eventEmitter.emit(GAME_TICK_EVENT, {
       alertStatuses: this.alertStatuses,
       tmode: this.tmode,
+      surrenderTimers: this.surrenderTimers,
     });
 
     this.botManager.setTMode(this.tmode);
